e018ac7237dfa70512cef2ea544fae86363d3408
alex-beaverg <a.beaverg@gmail.com>
2020-12-08 11:56:19 +0300

    tasks: 59/42

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -541,8 +541,12 @@ function distinct(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  const map = new Map();
+  array.map((v) => map.set(keySelector(v),
+    (map.get(keySelector(v)) === undefined ? []
+      : map.get(keySelector(v))).concat([valueSelector(v)])));
+  return map;
 }
 
 
@@ -559,8 +563,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.map(childrenSelector).flat();
 }
 
 
@@ -576,8 +580,8 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((prev, curr) => prev[curr], arr);
 }
 
 
@@ -599,8 +603,18 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const middle = (arr.length - 1) / 2;
+  let head = [];
+  let tail = [];
+  if (arr.length % 2 !== 0) {
+    head = arr.slice(0, middle);
+    tail = arr.slice(middle + 1);
+    return [...tail, arr[middle], ...head];
+  }
+  head = arr.slice(0, arr.length / 2);
+  tail = arr.slice(arr.length / 2);
+  return [...tail, ...head];
 }
 
 
